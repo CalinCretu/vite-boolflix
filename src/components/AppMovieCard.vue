8ffe302ec0f91fd82b0e77870ea6ec653e242415
CalinCretu <calincretu1412@gmail.com>
2023-11-13 17:59:25 +0100

    ADD - hover

--- src/components/AppMovieCard.vue
+++ src/components/AppMovieCard.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="card">
-    <div><img :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`" alt=""></div>
-    <ul>
-      <li>{{ item.title }}</li>
-      <li>{{ item.original_title }}</li>
+    <div><img class="bkg-img" :src="`https://image.tmdb.org/t/p/w342${item.poster_path}`" alt=""></div>
+    <ul class="info">
+      <li>Titolo: {{ item.title }}</li>
+      <li>Titolo Originale: {{ item.original_title }}</li>
       <li>
+        <span>Lingua: </span>
         <img height="10" v-if="srcFlag" :src="srcFlag" alt="">
-        <p v-else>{{ item.original_language }}</p>
+        <p v-else> {{ item.original_language }}</p>
       </li>
       <li v-if="(item.vote_average === '' || item.vote_average === 0)">
-        Nessuna recensione
+        Nessuna recensione ancora disponibile
       </li>
       <li v-else>
         <span>Voto :</span>
-        <div v-for="stars in  Math.round(item.vote_average / 2) "><span class="star"><font-awesome-icon
-              icon="fa-solid fa-star" /></span></div>
+        <span class="voto" v-for="stars in  Math.round(item.vote_average / 2) ">
+          <font-awesome-icon icon="fa-solid fa-star" />
+        </span>
       </li>
     </ul>
   </div>
@@ -45,11 +47,41 @@ export default {
 
 <style lang="scss" scoped>
 .card {
-  border: 2px solid black;
-  border-radius: 10px;
+  position: relative;
 
-  .star {
+  .bkg-img {
+    border-radius: 10px;
+    display: block;
+  }
+
+  .info {
+    display: none;
+    position: absolute;
+    padding: 8px;
+    color: white;
+    background-color: black;
+    border-radius: 10px;
+    top: 0;
+    height: 100%;
+  }
+
+  .voto {
+    width: 30px;
     color: gold;
+  }
+}
+
+.card>* {
+  line-height: 1.5rem;
+}
+
+.card:hover {
+  .bkg-img {
+    display: none;
+  }
+
+  .info {
+    display: block;
   }
 }
 </style>
